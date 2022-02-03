e67bb9bf4b783eff2d1d48354b0cc4336004b596
brunohsb <brunohsb1@gmail.com>
2022-02-03 15:42:55 -0300

    exercicio 4.1

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-JavaScript - Array e loop For/exercicio1.js
@@ -0,0 +1,25 @@
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+
+// for(let index = 0;index < groceryList.length ;index +=1){
+//     console.log(groceryList[index])
+// // }
+
+
+// let numeros = [1,2,3,4,5];
+// for(let numero of numeros) {
+//   console.log(numero);
+// }
+
+// // resultado: 
+// //1
+// //2
+// //3
+// //4
+// //5
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let pessoa of names){
+    console.log(pessoa)
+}
